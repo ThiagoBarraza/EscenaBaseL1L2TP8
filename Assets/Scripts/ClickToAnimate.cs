730e60adf6b244e656c674516da2d5be6bde2625
ThiagoBarraza <101569153+ThiagoBarraza@users.noreply.github.com>
2022-09-20 10:40:38 -0300

    U can open doors

---
+++ Assets/Scripts/ClickToAnimate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToAnimate : MonoBehaviour
+{
+    [SerializeField] Animator Anim;
+    [SerializeField] bool State;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if(State == false)
+        {
+            Anim.SetBool("Open", true);
+            Debug.Log(State);
+            State = true;
+        }
+        else
+        {
+            Anim.SetBool("Open", false);
+            Debug.Log(State);
+            State = false;
+        }
+
+    }
+}
